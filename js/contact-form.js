b8a4c9ccea3e8e1c576e0c40e750ac7b325289a2
fastexposure <fastexposure@yahoo.com>
2017-05-18 15:38:01 -0700

    contact form

--- js/contact-form.js
+++ js/contact-form.js
@@ -3,12 +3,13 @@
  --------------------------------------------- */
 $(document).ready(function(){
     $("#submit_btn").click(function(){
-        
+
         //get input field values
         var user_name = $('input[name=name]').val();
         var user_email = $('input[name=email]').val();
         var user_message = $('textarea[name=message]').val();
-        
+        var url = "./mail_handler.php"; // the script where you handle the form input.
+
         //simple validation at client's end
         //we simply change border color to red if empty field using .css()
         var proceed = true;
@@ -20,12 +21,12 @@ $(document).ready(function(){
             $('input[name=email]').css('border-color', '#e41919');
             proceed = false;
         }
-        
+
         if (user_message == "") {
             $('textarea[name=message]').css('border-color', '#e41919');
             proceed = false;
         }
-        
+
         //everything looks good! proceed...
         if (proceed) {
             //data to be sent to server
@@ -34,35 +35,39 @@ $(document).ready(function(){
                 'userEmail': user_email,
                 'userMessage': user_message
             };
-            
+
             //Ajax post data to server
-            $.post('contact_me.php', post_data, function(response){
-            
-                //load json data from server and output message     
-                if (response.type == 'error') {
-                    output = '<div class="error">' + response.text + '</div>';
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).closest('form').serialize(), // serializes the form's elements.
+                success: function (data) {
+                    $('.formSubmit').closest('form').find('input[type=text], textarea').val('');
+                    var messageAlert = 'alert-success';
+                    var messageText = 'Success, information was sent';
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable animated bounceIn"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    if (messageAlert && messageText) {
+                        $('#contact-form').find('.messages').html(alertBox);
+                    }
+                },
+                error: function(response){
+                    var messageAlert = 'alert-danger';
+                    var messageText = 'Failure, information was not sent';
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable animated bounceIn"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    if (messageAlert && messageText) {
+                        $('#contact-form').find('.messages').html(alertBox);
+                    }
                 }
-                else {
-                
-                    output = '<div class="success">' + response.text + '</div>';
-                    
-                    //reset values in all input fields
-                    $('#contact_form input').val('');
-                    $('#contact_form textarea').val('');
-                }
-                
-                $("#result").hide().html(output).slideDown();
-            }, 'json');
-            
+            });
         }
-        
+
         return false;
     });
-    
+
     //reset previously set border colors and hide all message on .keyup()
     $("#contact_form input, #contact_form textarea").keyup(function(){
         $("#contact_form input, #contact_form textarea").css('border-color', '');
         $("#result").slideUp();
     });
-    
+
 });
